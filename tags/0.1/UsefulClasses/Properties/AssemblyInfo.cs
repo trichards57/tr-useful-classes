ef3c4ddf17de7c2a49b0c6edf3c5cbb795fd491a
Trichards57@gmail.com <Trichards57@gmail.com@b2d090d4-a99a-c1bd-8660-c37900ea87bd>
2011-09-10 19:57:41 +0000

    Initial release.

---
+++ tags/0.1/UsefulClasses/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/// @namespace UsefulClasses.Exceptions
+/// @brief Exceptions used by the useful classes to represent problems.
+
+/// @namespace UsefulClasses
+/// @brief Basic namespace for all classes.
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UsefulClasses")]
+[assembly: AssemblyDescription("Useful classes to aid development.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Tony Richards")]
+[assembly: AssemblyProduct("Useful Classes")]
+[assembly: AssemblyCopyright("Copyright © Tony Richards 2011")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b10fd0ad-b7d8-4bb4-bd69-2b593f558d92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
+
+[assembly: CLSCompliant(true)]
---
+++ tags/0.1/UsefulClasses/StringExtension.cs
@@ -0,0 +1,117 @@
+﻿//***********************************************************************
+// Assembly         : UsefulClasses
+// Author           : Tony Richards
+// Created          : 08-29-2011
+//
+// Last Modified By : Tony Richards
+// Last Modified On : 09-10-2011
+// Description      : 
+//
+// Copyright (c) 2011, Tony Richards
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, this list
+// of conditions and the following disclaimer.
+//
+// Redistributions in binary form must reproduce the above copyright notice, this
+// list of conditions and the following disclaimer in the documentation and/or other
+// materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace UsefulClasses
+{
+    /// <summary>
+    /// Extension methods for the <see cref="String"/> class.
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Wraps the specified string in to a set of lines of specified length.
+        /// </summary>
+        /// <param name="value">The value to wrap.</param>
+        /// <param name="width">The width of the line.</param>
+        /// <returns>An <see cref="IEnumerable"/> containing the lines the string was wrapped on to.</returns>
+        public static IEnumerable<string> Wrap(this string value, int width)
+        {
+            var words = value.Explode(new[] { ' ' });
+
+            var curLineLength = 0;
+            var builder = new StringBuilder();
+            foreach (var w in words)
+            {
+                string word;
+                if (curLineLength + w.Length > width)
+                {
+                    if (curLineLength > 0)
+                    {
+                        yield return builder.ToString();
+                        builder.Clear();
+                        curLineLength = 0;
+                    }
+                    word = w.TrimStart();
+                }
+                else
+                    word = w;
+                
+                builder.Append(word);
+                curLineLength += word.Length;
+            }
+            yield return builder.ToString();
+        }
+
+        /// <summary>
+        /// Explodes the specified string.
+        /// </summary>
+        /// <param name="value">The string to explode.</param>
+        /// <param name="splitCharacters">The characters to split the string on.</param>
+        /// <returns>An <see cref="IEnumerable"/> containing the parts of the string.</returns>
+        public static IEnumerable<string> Explode(this string value, IEnumerable<char> splitCharacters)
+        {
+            var startIndex = 0;
+
+            while (true)
+            {
+                var index = value.IndexOfAny(splitCharacters.ToArray(), startIndex);
+                if (index == -1)
+                {
+                    yield return value.Substring(startIndex);
+                    yield break;
+                }
+
+                var word = value.Substring(startIndex, index - startIndex);
+                var nextChar = value.Substring(index, 1)[0];
+                if (char.IsWhiteSpace(nextChar))
+                {
+                    yield return word;
+                    yield return nextChar.ToString(CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    yield return word + nextChar;
+                }
+
+                startIndex = index + 1;
+            }
+        }
+    }
+}
